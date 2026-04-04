c741076c1b8de4772de778a2ff693e3e7231c5fa
DarMcB <dmcbrien2005@gmail.com>
2026-04-04 12:36:58 +0100

    Renamed "AppTopBar.kt" to "AppBar.kt" Created AppBottomBar Composable

--- app/src/main/java/com/example/opentimetrack/ui/AppBar.kt
+++ app/src/main/java/com/example/opentimetrack/ui/AppBar.kt
@@ -1,7 +1,15 @@
 package com.example.opentimetrack.ui
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.Icons.AutoMirrored.Filled
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -13,6 +21,7 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.opentimetrack.R
 import com.example.opentimetrack.ui.navigation.TimeNavHost
@@ -51,6 +60,41 @@ fun AppTopBar(
     )
 }
 
+@Composable
+fun AppBottomBar(
+    leftButtonName: String,
+    rightButtonName: String,
+    navigateToTimeInstance: (Int) -> Unit,
+    navigateToStatsScreen: (Int) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    BottomAppBar(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        IconButton(
+            onClick = { navigateToTimeInstance },
+            modifier = modifier.padding(horizontal = 60.dp)
+        ) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.List,
+                contentDescription = "",
+                modifier = modifier.fillMaxSize()
+            )
+        }
+        Spacer(Modifier.weight(1f))
+        IconButton(
+            onClick = { navigateToStatsScreen },
+            modifier = modifier.padding(horizontal = 60.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Info,
+                contentDescription = "",
+                modifier = modifier.fillMaxSize()
+            )
+        }
+    }
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
@@ -59,6 +103,19 @@ fun AppTopBarPreview() {
         AppTopBar(
             title = "App Title",
             canNavigateBack = false
+        )
+    }
+}
+
+@Preview
+@Composable
+fun AppBottomBarPreview() {
+    OpenTimeTrackTheme {
+        AppBottomBar(
+            leftButtonName = "List",
+            rightButtonName = "Stats",
+            navigateToStatsScreen = {},
+            navigateToTimeInstance = {}
         )
     }
 }
